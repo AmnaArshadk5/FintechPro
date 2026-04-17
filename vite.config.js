01fcff73e73e9218177f5bc7532095e0c6659b2c
AmnaArshadk5 <amnaarshadk5@gmail.com>
2026-04-18 02:24:51 +0500

    Setup: GitHub Actions for auto-deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Replace 'FintechPro' with your exact repository name if it's different
+
   base: '/FintechPro/', 
   server: {
     proxy: {
